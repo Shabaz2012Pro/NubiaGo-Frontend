4f1fa44ef0cc762674c4957a207be589f4d3e0dc
Shabaz2012Pro <150820902+Shabaz2012Pro@users.noreply.github.com>
2025-06-27 09:31:52 +0300

    Updated SizeGuidePage.tsx

--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -4,4 +4,3 @@ export { AuthModal } from './AuthModal';
 export { ForgotPasswordForm } from './ForgotPasswordForm';
 export { SignInForm as LoginForm } from './SignInForm';
 export { SignUpForm as RegisterForm } from './SignUpForm';
-export { default as TwoFactorForm } from '../molecules/TwoFactorSetup';
